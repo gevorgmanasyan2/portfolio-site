1be7147f6743fd9be4150845fb0fe170d6c38b79
gevorgmanasyan2 <gevorgmanasyan2@gmail.com>
2021-05-04 19:03:22 +0400

    first commit

---
+++ src/components/PortfolioItemPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PortfolioItemPage=(props)=>(
+    <div>
+        <h1>A Thing I ve Done</h1>
+        <p>This page is for the item with the of {props.match.params.id}</p>
+    </div>
+);
+ export default PortfolioItemPage;
